b29c0b8fe0a903da0677c830fedae648051cd808
yonathanm10 <yonathanm@cocoindonesia.com>
2024-05-10 11:17:49 +0700

    accordion-1

---
+++ src/pages/accordion/data.js
@@ -0,0 +1,19 @@
+const data = [
+    {
+      id: 1,
+      question: "What is the capital of France?",
+      answer: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo odio libero temporibus rerum, et unde amet impedit nihil debitis excepturi!" 
+    },
+    {
+      id: 2,
+      question: "What is the largest planet in our solar system?",
+      answer: "Jupiter Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo odio libero temporibus rerum, et unde amet impedit nihil debitis excepturi! last"
+    },
+    {
+      id: 3,
+      question: "Who wrote 'Romeo and Juliet'?",
+      answer: "William Shakespeare Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo odio libero temporibus rerum, et unde amet impedit nihil debitis excepturi! last not"
+    },
+  ]
+
+  export default data 
